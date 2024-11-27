512ff20696f08a7b00dffaa4074d3eb33da2b6a6
Agadyr <kairambekovmadi@gmail.com>
2024-11-27 17:14:34 +0500

    EASY task - Implemented function to calculate the number of rotations needed to match two strings.

--- module-a/04-Task/js/shiftedDiff.js
+++ module-a/04-Task/js/shiftedDiff.js
@@ -6,6 +6,29 @@
  * @return {Number}
  */
 
-function shiftedDiff(first, second) {
-	/* Work here */
+function shiftedDiff() {
+	const message = document.getElementById('message')
+
+	let string1 = document.getElementById('string1').value
+	let string2 = document.getElementById('string2').value
+
+	if (string1.length !== string2.length) {
+		message.innerHTML = `This strings not equal by length -1`
+		return null
+	}
+
+	let count = 0
+
+    while (count < string1.length) {
+        if (string1 === string2) {
+            message.innerHTML = `${count} shifts`;
+            return count;
+        }
+
+        string1 = string1[string1.length - 1] + string1.slice(0, -1);
+        count++;
+    }
+
+    message.innerHTML = `Strings cannot be matched by rotation: -1`;
+	return -1
 }

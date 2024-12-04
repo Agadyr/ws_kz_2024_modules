a92856bd2a2dd5737bb4093d4342810604a05237
Agadyr <kairambekovmadi@gmail.com>
2024-12-04 18:06:00 +0500

    Add register and login functionality, including request handling in controllers and services

--- module-b/app/Http/Controllers/v1/AuthController.php
+++ module-b/app/Http/Controllers/v1/AuthController.php
@@ -3,13 +3,35 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Validator;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-//    public function register(Request $request): \Illuminate\Http\JsonResponse
-//    {
-//
-//    }
+    protected AuthService $authService;
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+    public function register(RegisterRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $data = $request->validated();
+
+        $result = $this->authService->register($data['name'], $data['password']);
+
+        return response()->json($result, 201);
+    }
+
+    public function login(LoginRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $data = $request->validated();
+
+        $result = $this->authService->login($data['name'], $data['password']);
+
+        if (isset($result['status'])) {
+            return response()->json($result, 401);
+        }
+        return response()->json($result, 200);
+    }
 }
---
+++ module-b/app/Services/AuthService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService
+{
+    public function register(string $name, string $password): array
+    {
+        $user = \App\Models\User::create([
+            'name' => $name,
+            'password' => Hash::make($password),
+        ]);
+
+        $token = $user->createToken('nomad')->accessToken;
+
+        return [
+            'user' => $user,
+            'role' => 'User',
+            'token' => $token
+        ];
+    }
+
+    public function login(string $name, string $password): array
+    {
+        $role = 'user';
+        if (Auth::attempt(['name' => $name, 'password' => $password])) {
+            $user = Auth::user();
+
+            $token = $user->createToken('nomad')->accessToken;
+
+            if ($user->is_admin === 1) {
+                $role = 'Admin';
+            }
+
+            return [
+                'user' => $user->name,
+                'role' => $role,
+                'token' => $token
+            ];
+        }
+
+        return [
+            'message' => "user $name is unauthorized",
+            'status' => 401
+        ];
+    }
+}
